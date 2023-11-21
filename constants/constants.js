6f1eb6d40a036db548623f056a733a0a8b8d5d87
ashvinck <ashvinck10@gmail.com>
2023-11-21 19:53:57 +0530

    added netlify api

--- constants/constants.js
+++ constants/constants.js
@@ -1,3 +1,3 @@
 export const DB_NAME = 'easynotes';
-export const API = 'http://localhost:5173';
+export const API = 'https://easynotes-10.netlify.app';
 export const USER_TEMPORARY_TOKEN_EXPIRY = 20 * 60 * 1000; // 20 minutes
